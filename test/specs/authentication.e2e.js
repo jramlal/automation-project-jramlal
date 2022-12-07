e24191f313bf6bfe92970f7ae39042447b01b651
Jonelle Ramlal <jramlal98@gmail.com>
2022-12-07 17:09:29 -0500

    Completed Authentication

--- test/specs/authentication.e2e.js
+++ test/specs/authentication.e2e.js
@@ -1,6 +1,8 @@
 
 
-const AuthPage = require('../pageobjects/auth.page')
+const AuthPage = require('../pageobjects/auth.page');
+const { faker } = require('@faker-js/faker');
+
 
 describe('Authentication Test Scenarios', () => {
     it('should login with valid credentials', async () => {
@@ -8,9 +10,36 @@ describe('Authentication Test Scenarios', () => {
 
         await AuthPage.login('luffy@onepiece.com', 'Password123!');
 
-        await expect(browser).toHaveUrl("https://ui-automation-camp.vercel.app/products")
-        await expect(AuthPage.btnSignOut).toBeDisplayed()
+        await expect(browser).toHaveUrl("https://ui-automation-camp.vercel.app/products");
+        await expect(AuthPage.btnSignOut).toBeDisplayed(); 
+
+        await AuthPage.signOut();
     })
+
+    it('should create an account with credentials gerenated by faker', async () => { 
+
+        const email = faker.internet.email();
+        const password = (faker.random.alphaNumeric(8) + "!");
+
+        await AuthPage.open(); 
+
+        await AuthPage.signUp(email, password);
+
+        await expect(browser).toHaveUrl("https://ui-automation-camp.vercel.app/products");
+        await expect(AuthPage.btnSignOut).toBeDisplayed(); 
+
+        await AuthPage.signOut(); 
+    })
+
+    it('should fail to create an account with no password', async () => { 
+
+        const email = faker.internet.email();
+
+        await AuthPage.open(); 
+
+        await AuthPage.signUp(email, " ");
+
+        await expect(AuthPage.errorMsg).toBeDisplayed();
+    })
+    
 })
-
-
