eecdde23fd1487471bde92c14cd719b33184602e
Jonelle Ramlal <jramlal98@gmail.com>
2022-12-07 17:15:39 -0500

    Updated Login Credentials

--- test/specs/authentication.e2e.js
+++ test/specs/authentication.e2e.js
@@ -8,7 +8,7 @@ describe('Authentication Test Scenarios', () => {
     it('should login with valid credentials', async () => {
         await AuthPage.open(); 
 
-        await AuthPage.login('luffy@onepiece.com', 'Password123!');
+        await AuthPage.login('hansel@catfish.com', 'Password123!');
 
         await expect(browser).toHaveUrl("https://ui-automation-camp.vercel.app/products");
         await expect(AuthPage.btnSignOut).toBeDisplayed(); 
